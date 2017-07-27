22aaa4c53be0f43c1cfb2bca6b6ebec1e790c0df
Jimmy Garcia <cooljj_20@sbcglobal.net>
2017-07-26 21:04:25 -0500

    Added find new home search box

--- realty-app/src/app/coming-soon/coming-soon.component.ts
+++ realty-app/src/app/coming-soon/coming-soon.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./coming-soon.component.css']
 })
 export class ComingSoonComponent implements OnInit {
+  searchTerm: string = "";
 
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  onSearch() {
+    window.open('http://www.har.com/search/dosearch?for_sale=1&quicksearch=' + encodeURI(this.searchTerm));
   }
 
 }
